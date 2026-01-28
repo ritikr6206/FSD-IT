94fcc1cdbb93905c47f7a1deb4d3791c4d4ff44d
ritikr6206 <ritikr6206@gmail.com>
2026-01-28 15:35:38 +0530

    fetch api revision

--- day_01/promise/index.js
+++ day_01/promise/index.js
@@ -1,13 +1,23 @@
-const mypromise = new Promise((resolve, reject) => {
-    let marks =80;
-    if (marks >= 60) {
-        resolve("You are passed");
-    } else {
-        reject("You are failed");
-    }
-})
-mypromise.then((message) => {
-    console.log(message);
-}).catch((message) => {
-    console.log(message);
-});
+const getData = async()=>{
+    const response = await fetch('https://fakestoreapi.com/products');
+    const data = await response.json();
+    console.log(data);
+}
+
+getData();
+
+
+
+// const mypromise = new Promise((resolve, reject) => {
+//     let marks =80;
+//     if (marks >= 60) {
+//         resolve("You are passed");
+//     } else {
+//         reject("You are failed");
+//     }
+// })
+// mypromise.then((message) => {
+//     console.log(message);
+// }).catch((message) => {
+//     console.log(message);
+// });
